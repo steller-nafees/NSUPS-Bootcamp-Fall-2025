f8c582c4d2c98ebc8bfcd1b36975b5773b75c68f
steller-nafees <nafeesnislam.official@gmail.com>
2025-10-30 21:39:17 +0600

    Delete folder Second Order Statistics

---
+++ Bootcamp Week 1 Contest/StringStream/code/StringStream.cpp
@@ -0,0 +1,121 @@
+//============================================================================
+// Platform      : Codeforces 
+// Problem Title : StringStream
+// Author        : H M Nafees N Islam
+// Institution   : North South University (NSU), Department of CSE
+// Date          : October 30, 2025 
+//
+// Problem Statement:
+// 
+//
+// Input:
+//
+// Output:
+//
+// Constraints:
+//  - Time limit: 
+//  - Memory limit: 
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define OUTPUT_CONDITION(x) x?"YES" : "NO"
+#define INPUT_ARRAY(a,n) for(int i = 0; i<n; i++) cin>>a[i];
+#define OUTPUT_2Values(o1,o2) cout<<o1<<" "<<o2<<endl
+#define SORT_DESC(a) sort(a.begin(), a.end(),greater<int>())
+#define SORT_ASC(a) sort(a.begin(), a.end())
+#define vi(a,n) vector<int> a(n);
+#define vl(a,n) vector<ll> a(n);
+#define INPUT_int(n) int n; cin>>n;
+#define INPUT_ll(n) ll n; cin>>n;
+#define INPUT_string(s) string s; cin>>s;
+#define INPUT_char(c) char c; cin>>c;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define STR_LEN(s) ((int)s.size())
+#define STR_REV(s) reverse(all(s))
+#define STR_SORT_ASC(s) sort(all(s))
+#define STR_SORT_DESC(s) sort(rall(s))
+#define STR_TOLOWER(s) transform(all(s), s.begin(), ::tolower)
+#define STR_TOUPPER(s) transform(all(s), s.begin(), ::toupper)
+#define STR_CONTAINS(s, sub) (s.find(sub) != string::npos)
+#define STR_SUBSTR(s, l, r) (s.substr(l, r))
+#define STR_APPEND(s1, s2) s1 += s2
+#define STR_STARTS_WITH(s, pref) (s.rfind(pref, 0) == 0)
+#define STR_ENDS_WITH(s, suff) (s.size() >= suff.size() && s.compare(s.size() - suff.size(), suff.size(), suff) == 0)
+#define STR_SPLIT(vec, s, delim) { string tmp; stringstream ss(s); while (getline(ss, tmp, delim)) vec.pb(tmp); }
+#define STR_JOIN(res, vec, delim) { res = ""; for (int i = 0; i < (int)vec.size(); i++) { res += vec[i]; if (i + 1 != (int)vec.size()) res += delim; } }
+#define STR_TO_INT(s) stoi(s)
+#define STR_TO_LL(s) stoll(s)
+#define INT_TO_STR(x) to_string(x)
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << '\n';
+#else
+#define debug(x)
+#endif
+
+vector<int> parsing(string s)
+{
+    vector <int> result;
+    stringstream ss(s);
+
+    int number; 
+    char comma;
+
+    while(ss >> number)
+    {
+        result.push_back(number);
+        ss >> comma;
+    }
+
+    return result;
+}
+
+
+void solve()
+{
+    INPUT_string(s);
+    vector<int> numbers = parsing(s);
+
+    int n = numbers.size();
+    for(int i =0 ; i<n ;i++)
+    {
+        cout<<numbers[i] <<endl;
+    }
+}
+
+int main()
+{
+    FAST_IO;
+
+    //int t; cin>>t; 
+    //while (t--) solve();
+    
+    //For single test case, comment the above 3 lines and uncomment below line
+    solve();
+
+    return 0;
+}
+
+/*
+   Solution Logic:
+  - 
+*/
+
+/*
+  Alhamdulillah, problem solved successfully!
+  الحمد لله الذي بنعمته تتم الصالحات
+*/
